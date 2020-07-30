6b99032a181590b0ade035421c02b725e82dae10
Inioluwa OluAlakija <inioluwa4@gmail.com>
2020-07-30 13:51:18 -0400

    Login Story Completed

--- notesharing/src/app/app.module.ts
+++ notesharing/src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
