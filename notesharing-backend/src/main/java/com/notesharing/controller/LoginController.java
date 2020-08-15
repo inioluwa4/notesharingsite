cfa96dab8965c9d6d5ca3e9b07117025d3345a9e
Inioluwa OluAlakija <inioluwa4@gmail.com>
2020-08-15 19:16:26 -0400

    m

--- notesharing-backend/src/main/java/com/notesharing/controller/LoginController.java
+++ notesharing-backend/src/main/java/com/notesharing/controller/LoginController.java
@@ -20,7 +20,7 @@ import com.notesharing.models.Login;
 import com.notesharing.services.hibernate.LoginService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 
 public class LoginController {
 
